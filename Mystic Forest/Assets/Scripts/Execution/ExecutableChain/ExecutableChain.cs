1e82f7a1ffccdb89e2fa9c7723fb2279742c6009
harri471 <kemar.harris@mail.utoronto.ca>
2020-04-29 07:46:37 -0400

    A very bare bones version of chain added

---
+++ Mystic Forest/Assets/Scripts/Execution/ExecutableChain/ExecutableChain.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public interface IExecutableChain : IEnumerable<IExecutable>// , DirectionPickable 
+{
+    // ApproachAnimationData approach { get; }
+
+    //RetreatAnimationData retreat { get; }
+
+    IExecutable head { get; }
+
+    //AttackChainExecutionVisual getVisual(Battler attacker);
+
+}
---
+++ Mystic Forest/Assets/Scripts/Execution/ExecutableChain/ExecutableChainSO.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Collections;
+
+[CreateAssetMenu()]
+public class ExecutableChainSO : ScriptableObject, IExecutableChain //, ExecutableAttackChain
+{
+    // Amount of time in seconds the chain takes to execute
+    public ExecutableSO[] attacks;
+
+    public IExecutable head => attacks[0];
+
+    public IEnumerator<IExecutable> GetEnumerator()
+    {
+        for (int i = 0; i < attacks.Length; i ++)
+        {
+            yield return attacks[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return attacks.GetEnumerator();
+    }
+}
