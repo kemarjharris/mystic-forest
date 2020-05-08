c6ee9cc5962c3621b65a2ef1930c75d5c10a3d3a
harri471 <kemar.harris@mail.utoronto.ca>
2020-05-08 08:21:34 -0400

    Execution tested and done

---
+++ Mystic Forest/Assets/Tests/ExecutionTests/ChainInputReaderTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NSubstitute;
+
+namespace ChainInputReaderTest
+{
+    public class ChainInputReaderTest
+    {
+        ChainInputReader reader = new ChainInputReader();
+
+        [SetUp]
+        public void SetUp()
+        {
+            reader = new ChainInputReader();
+            IUnityInputService service = Substitute.For<IUnityInputService>();
+            foreach (DirectionCommandButton button in EnumUtil.values<DirectionCommandButton>())
+            {
+                if (button == DirectionCommandButton.NULL) continue;
+                string key = button.ToString().ToLower();
+                service.GetKeyDown(key).Returns(false);
+                service.GetKey(key).Returns(false);
+                service.GetKeyUp(key).Returns(false);
+            }
+            reader.service = service;
+        }
+
+        [Test]
+        public void ReadsGetKeyAcceptableInputTest()
+        {
+            reader.service.GetKey("z").Returns(true);
+            Assert.AreEqual("z", reader.ReadInput());
+        }
+
+        [Test]
+        public void ReadsKeyUpAcceptableInputTest()
+        {
+            reader.service.GetKeyUp("z").Returns(true);
+            Assert.AreEqual("z", reader.ReadInput());
+        }
+
+        [Test]
+        public void IgnoresUnacceptableInputTest()
+        {
+            reader.service.GetKey("space").Returns(true);
+            Assert.AreEqual("", reader.ReadInput());
+        }
+
+        [Test]
+        public void ReturnsMultipleInputTest()
+        {
+            reader.service.GetKey("z").Returns(true);
+            reader.service.GetKey("x").Returns(true);
+            string expected = reader.ReadInput();
+            Assert.True(expected.Equals("xz") || expected.Equals("zx"));
+        }
+    }
+}
---
+++ Mystic Forest/Assets/Tests/ExecutionTests/ExecutableChainTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace ExecutableChainTest
+{
+    public class ExecutableChainTest
+    {
+        // Different Enumerators return different things
+        [Test]
+        public void DifferentEnumeratorsGiveDifferentObjectsTest()
+        {
+            PressExecutableSO press = ScriptableObject.CreateInstance<PressExecutableSO>();
+            ExecutableChainSO chain = ScriptableObject.CreateInstance<ExecutableChainSO>();
+            chain.attacks = new ExecutableSO[] { press };
+            IEnumerator enumOne = chain.GetEnumerator();
+            IEnumerator enumTwo = chain.GetEnumerator();
+            enumOne.MoveNext();
+            enumTwo.MoveNext();
+            Assert.AreNotSame(enumOne.Current, enumTwo.Current);
+        }
+
+    }
+}
