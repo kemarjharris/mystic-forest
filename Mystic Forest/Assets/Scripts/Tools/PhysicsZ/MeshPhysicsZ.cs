c5eec1ae449729bdcea29bf4b6bb723172a1b941
harri471 <kemar.harris@mail.utoronto.ca>
2020-05-28 20:04:26 -0400

    Two Battler colliders will no longer overlap

--- Mystic Forest/Assets/Scripts/Tools/PhysicsZ/MeshPhysicsZ.cs
+++ Mystic Forest/Assets/Scripts/Tools/PhysicsZ/MeshPhysicsZ.cs
@@ -35,13 +35,20 @@ public class MeshPhysicsZ : MonoBehaviour
             HandleGroundCollision();
         }
 
-        // Case for landing on a battler
-        else if (!IsGrounded && collision.gameObject.tag == "Battler")
+        // Case for landing in a battler
+        else if (collision.gameObject.tag == "Battler" && collider.bounds.Contains(collision.transform.position)) 
         {
-            float temp = transform.position.x;
-            //transform.position = new Vector3(collision.transform.position.x, transform.position.y, transform.position.z);
-            //collision.transform.position = new Vector3(temp, collision.transform.position.y, collision.transform.position.z);
-            //HandleLandOnBattlerCollision(collision);
+            Debug.Log("purr");
+            float colliderCenter = transform.position.x + collider.center.x;
+            Vector3 push = new Vector3(collider.size.x, 0, 0);
+
+            if (colliderCenter > collision.transform.position.x) push *= -1;
+        
+            transform.position -= push;
+            collision.transform.position += push;
+
+
+            //rb.velocity += rb;
         }
     }
 
@@ -49,66 +56,6 @@ public class MeshPhysicsZ : MonoBehaviour
     private void HandleGroundCollision()
     {
         rb.useGravity = false;
-    }
-
-    private void HandleLandOnBattlerCollision(Collision collision)
-    {
-        float contactCount = collision.contactCount;
-        if (contactCount != 4)
-        {
-            return;
-        }
-        // Get first cotnact point to store point
-        float xDistance = 0.2f;
-        // Battlers have box colliders, so collision point will be a rectangle.
-
-        // assuming unity doesnt gives me diagonal points
-
-
-        Vector3 pointA = collision.GetContact(0).point;
-        Vector3 pointB = collision.GetContact(1).point;
-        int i = 2;
-        while (i < contactCount && pointA.x == pointB.x)
-        {
-            pointB = collision.GetContact(i).point;
-            i += 1;
-        }
-        
-
-        if (pointA.y != pointB.y)
-        {
-            Debug.LogWarning("Running code to push two battlers apart when landing, but given points do not form a rectangle");
-            Debug.Log(pointA + " " + pointB);
-            return;
-            
-        }
-
-        xDistance += Mathf.Abs(pointB.x - pointA.x);
-
-        Vector3 seperation = new Vector3(xDistance, 0, 0);
-        Debug.Log(pointA + " " + pointB);
-        Debug.Log(seperation);
-        // Push objects apart depending on which object is on the left and the right of the collision
-        if (gameObject.transform.position.x <= collision.transform.position.x)
-        {
-            collision.transform.position += seperation;
-        } else
-        {
-            collision.transform.position -= seperation;
-        }
-    }
-
-    private void ThrowIncorrectContactException(Collision collision)
-    {
-        IList<Vector3> list = new List<Vector3>();
-        for (int i = 0; i < collision.contactCount; i++)
-        {
-            list.Add(collision.GetContact(i).point);
-        }
-        throw new System.Exception(
-            "Collision with battler occured on a battler's head, but the resulting collision was not a rectangle. The collision points in question: "
-            + CollectionUtils.Print<Vector3>(list)
-            );
     }
 
     private void OnCollisionExit(Collision collision)
@@ -121,10 +68,6 @@ public class MeshPhysicsZ : MonoBehaviour
         else if (!IsGrounded && collision.gameObject.tag == "Battler")
         {
             float temp = transform.position.x;
-            //collider.enabled = false;
-            //transform.position = new Vector3(collision.transform.position.x, transform.position.y, transform.position.z);
-            //collision.transform.position = new Vector3(temp, collision.transform.position.y, collision.transform.position.z);
-            //HandleLandOnBattlerCollision(collision);
         }
     }
 
@@ -171,41 +114,37 @@ public class MeshPhysicsZ : MonoBehaviour
         if (IsGrounded) // apply drag
         {
             rb.velocity *= 1 - dragFactor;
-        } else
+        }
+        else
         {
             bool collided = CheckUnderBattler(out RaycastHit hitInfo);
-            if (collided && rb.velocity.y < 0 && hitInfo.collider.gameObject.tag == "Battler" )
-            {
-                Debug.Log("landed on battler");
-                //hitInfo.collider.transform.position += new Vector3(-rb.velocity.x/ 2, 0, 0);
-                //hitInfo.collider.enabled = false;
-                collider.enabled = false;
-            } else if (hitInfo.collider == ground)
+            if (hitInfo.collider == ground)
             {
                 collider.enabled = true;
             }
+            if (collided && rb.velocity.y < 0 && hitInfo.collider.gameObject.tag == "Battler" )
+            {
+             //   SeparateBattlers(hitInfo.collider, hitInfo.point);
+                
+                collider.enabled = false;
+            } 
             
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        BoxColliderDrawer.DrawBoxCollider(transform, Color.magenta, collider.center, collider.size);
+        
     }
 
     private void OnDrawGizmosSelected()
     {
         Vector3 scaled = Vector3.Scale(collider.size, gameObject.transform.localScale);
-        BoxCastVisualizer.DrawBoxCastBox(transform.position + collider.center + Vector3.down * 0.5f, new Vector3(scaled.x + 0.2f, 1, scaled.z) / 2, transform.rotation, Vector3.down, 0, Color.cyan);
+        BoxColliderDrawer.DrawBoxCollider(transform, Color.magenta, collider.center, collider.size);
+        BoxCastVisualizer.DrawBoxCastBox(transform.position + collider.center + Vector3.down * 0.5f, new Vector3(scaled.x, 1, scaled.z) / 2, transform.rotation, Vector3.down, 0, Color.cyan);
     }
 
     private bool CheckUnderBattler(out RaycastHit hit)
     {
         Vector3 scaled = Vector3.Scale(collider.size, gameObject.transform.localScale);
-        bool collided = Physics.BoxCast(transform.position + collider.center * 0.5f, new Vector3(scaled.x + 0.2f, 1, scaled.z) / 2, Vector3.down, out RaycastHit hitInfo, gameObject.transform.rotation, 0.5f);
+        bool collided = Physics.BoxCast(transform.position + collider.center * 0.5f, new Vector3(scaled.x, 1, scaled.z) / 2, Vector3.down, out RaycastHit hitInfo, gameObject.transform.rotation, 0.5f);
         hit = hitInfo;
         return collided;
     }
-
-
 }
