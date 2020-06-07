dae4e6f881925e2a4a64026db8dda75aa83b1e5c
harri471 <kemar.harris@mail.utoronto.ca>
2020-06-07 01:53:47 -0400

    Hit mesh added for Lock on area

---
+++ Mystic Forest/Assets/Scripts/HitBox/HitMesh.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System;
+
+[RequireComponent(typeof(MeshCollider))]
+[ExecuteInEditMode]
+public class HitMesh : MonoBehaviour, IHitBox
+{
+    new MeshCollider collider;
+    HashSet<Collider> currentlyColliding;
+
+    public void Awake()
+    {
+        collider = GetComponent<MeshCollider>();
+        currentlyColliding = new HashSet<Collider>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        currentlyColliding.Add(collider);
+    }
+
+    public void OnTriggerExit(Collider collider)
+    {
+        currentlyColliding.Remove(collider);
+    }
+
+    public void CheckCollision(Action<Collider> onCollide)
+    {
+        currentlyColliding.RemoveWhere((collider) => collider == null);
+        foreach (Collider collider in  currentlyColliding)
+        {
+            onCollide.Invoke(collider);
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireMesh(collider.sharedMesh, transform.position, transform.rotation, transform.localScale);
+    }
+}
