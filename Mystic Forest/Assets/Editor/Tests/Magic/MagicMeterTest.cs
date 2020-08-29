64e406179cb01b76a88de0badab281706f696af0
harri471 <kemar.harris@mail.utoronto.ca>
2020-08-29 07:20:21 -0400

    Magic meter tests

---
+++ Mystic Forest/Assets/Editor/Tests/Magic/MagicMeterTest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NSubstitute;
+
+namespace Tests
+{
+    public class MagicMeterTest
+    {
+        MagicMeter meter;
+        MagicMeterSettings settings;
+        // magic meter increments
+        // magic meter decreaaes with time
+
+        [SetUp]
+        public void SetUp()
+        {
+            meter = new GameObject().AddComponent<MagicMeter>();
+            settings = ScriptableObject.CreateInstance<MagicMeterSettings>();
+            settings.decreasePerSecond = 1;
+            settings.incrementBy = 10;
+            settings.maxMana = 100;
+            meter.settings = settings;
+        }
+        
+
+        [Test]
+        public void OnBattlerHit_BattlerHit_MagicMeterIncreasesTest()
+        {
+            IBattler battler = Substitute.For<IBattler>();
+            meter.OnBattlerHit(battler);
+            Assert.AreEqual(settings.incrementBy, meter.Value);
+        }
+
+        [Test]
+        public void OnBattlerHit_MagicMeterIncrements_BoundedByMaxMana()
+        {
+            IBattler battler = Substitute.For<IBattler>();
+            settings.incrementBy = settings.incrementBy = settings.maxMana + 1;
+            meter.OnBattlerHit(battler);
+            Assert.AreEqual(settings.maxMana, meter.Value);
+        }
+
+        [UnityTest]
+        public IEnumerator Update_MagicMeterDecreasesByDecreaseByTest()
+        {
+            IBattler battler = Substitute.For<IBattler>();
+            meter.OnBattlerHit(battler);
+            yield return new WaitForSeconds(settings.decreasePerSecond);
+            // decreased fast enough
+            Assert.Less(meter.Value, settings.incrementBy - (settings.decreasePerSecond * 0.9f));
+            // decreased slowly
+            Assert.Greater(meter.Value, settings.incrementBy - (settings.decreasePerSecond * 1.1f));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.Destroy(meter.gameObject);
+        }
+    }
+}
--- Mystic Forest/Assets/Scripts/Magic/MagicMeter.cs
+++ Mystic Forest/Assets/Scripts/Magic/MagicMeter.cs
@@ -7,18 +7,24 @@ public class MagicMeter : MonoBehaviour
     public ValueBar bar;
     BoundedValue<float> value;
     public MagicMeterSettings settings;
+    public float Value => value.Value;
 
-    private void Awake()
+    private void Start()
     {
         value = new BoundedValue<float>(0, 0, settings.maxMana);
-        bar.Construct(value);
+        if (bar != null) bar.Construct(value);
 
         GameObject[] battlers = GameObject.FindGameObjectsWithTag("Battler");
         for (int i = 0; i < battlers.Length; i++)
         {
             Battler b = battlers[i].GetComponent<Battler>();
-            b.eventSet.onBattlerHit += (battler) => value.Value += settings.incrementBy;
+            b.eventSet.onBattlerHit += OnBattlerHit;
         }
+    }
+
+    public void OnBattlerHit(IBattler battler)
+    {
+        value.Value += settings.incrementBy;
     }
 
     private void Update()
